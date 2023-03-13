5b94b0231345476e0310434296d2c62278836868
kodeman2 <holuwakoladey@gmail.com>
2023-03-13 12:07:27 +0100

    fix:ui-responsiveness changes

--- src/views/Errorpage.vue
+++ src/views/Errorpage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 text-white">
         <h1>404</h1>
         <p>Page not found</p>
       </div>
