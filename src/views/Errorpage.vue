5cd20d58d8c5fab94006d9d1ab59dc0a2d11cd75
kodeman2 <holuwakoladey@gmail.com>
2023-03-15 20:10:10 +0100

    feat:added error page

--- src/views/Errorpage.vue
+++ src/views/Errorpage.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-12 text-white">
-        <h1>404</h1>
-        <p>Page not found</p>
+    <div class="flex items-center justify-center mt-[120px]">
+      <div class="flex flex-col text-white gap-5 items-center justify-center">
+        <img src="../assets/error.svg" alt="404" />
+        <h1 class="text-4xl font-semibold">Oops!</h1>
+        <p class="text-xl lg:w-[500px]">Something went wrong. The page you are looking for might have been removed or temporarily unavailable.</p>
+        <router-link to="/">
+          <button class="bg-[#0cce6b] text-[#000] text-base font-medium px-8 py-3 rounded-lg hover:bg-[#98f5c5] ">
+            Back to Home
+          </button>
+        </router-link>
       </div>
     </div>
   </div>
